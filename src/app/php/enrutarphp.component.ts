5242fae72486374df1e43f51b7c1e2d10da2ff24
orgralu <62081979+orgralu@users.noreply.github.com>
2022-12-11 16:48:19 -0500

    Delete src directory

--- src/app/php/enrutarphp.component.ts
+++
@@ -1,8 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  templateUrl: './indexphp.html'
-})
-export class indexPHP {
-
-}
--- src/app/servicio/Empleado.ts
+++
@@ -1,9 +0,0 @@
-export interface Empleado{
-    id? : string,
-    nombre : string,
-    correo : string,
-    apellido : string,
-    cedula : string,
-    id_cedula : string,
-    ntipocedula : string
-}
